ab581eef90abe258e93bd7effbb264a187c4f28c
Jerry Tegno <jerrytegno@gmail.com>
2018-04-20 21:04:59 -0700

    organize init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,6 +8,10 @@
 
 ;;; Code:
 
+;;;;;;;;;;;;;;;;;;
+;;; Misc settings:
+;;;;;;;;;;;;;;;;;;
+
 ;; redirect custom options to an unused tmp file
 (setq custom-file (make-temp-file "custom.el"))
 
@@ -30,8 +34,17 @@
     (add-to-list 'default-frame-alist '(font . "-*-Input-normal-normal-ultracondensed-*-15-*-*-*-m-0-iso10646-1")))
 
 ;; pipe backup files elsewhere
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
-(setq backup-by-copying t)
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups"))
+      backup-by-copying t)
+
+;; better scrolling defaults
+(setq scroll-margin 10
+      scroll-preserve-screen-position 1)
+
+
+;;;;;;;;;;;;;;;;;;
+;;; Package setup:
+;;;;;;;;;;;;;;;;;;
 
 ;; setup package lists
 (require 'package)
@@ -50,74 +63,80 @@
   (package-install 'use-package))
 (require 'use-package)
 
+;; install packages declared by default
+(setq use-package-always-ensure t)
+
+;; auto-package-update
+;; automatically update declared packages
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t
+	auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
+
+;;;;;;;;;;;
+;;; Themes:
+;;;;;;;;;;;
+
 ;; themes
-(use-package solarized-theme :ensure t :config (load-theme 'solarized-light t))
-;; (use-package base16-theme :ensure t :config (load-theme 'base16-gruvbox-dark-soft t))
-;; (use-package challenger-deep-theme :ensure t :config (load-theme 'challenger-deep t))
-;; (use-package gruvbox :ensure t :config (load-theme 'gruvbox-dark-soft t))
-;; (use-package doom-themes :ensure t :config (load-theme 'doom-spacegrey t))
-;; (use-package zenburn-theme :ensure t :config (load-theme 'zenburn t))
-;; (use-package color-theme-sanityinc-tomorrow :ensure t :config (load-theme 'color-theme-sanityinc-tomorrow-night t))
-;; (use-package kaolin-themes :ensure t :config (load-theme 'kaolin-dark t))
+;; (use-package solarized-theme :config (load-theme 'solarized-light t))
+;; (use-package base16-theme :config (load-theme 'base16-gruvbox-dark-soft t))
+;; (use-package challenger-deep-theme :config (load-theme 'challenger-deep t))
+;; (use-package gruvbox :config (load-theme 'gruvbox-dark-soft t))
+(use-package doom-themes :config (load-theme 'doom-one t))
+;; (use-package zenburn-theme :config (load-theme 'zenburn t))
+;; (use-package color-theme-sanityinc-tomorrow :config (load-theme 'color-theme-sanityinc-tomorrow-night t))
+;; (use-package kaolin-themes :config (load-theme 'kaolin-dark t))
+;; (use-package dracula-theme :config (load-theme 'dracula t))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Misc/Convenience packages:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; exec-path-from-shell
 (use-package exec-path-from-shell
-  :ensure t
   :config
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize)))
 
-;; elm-mode
-(use-package elm-mode
-  :ensure t
-  :config (setq elm-format-on-save t))
+;; avy
+(use-package avy)
 
-;; rust-mode
-(use-package rust-mode
-  :ensure t
-  :config (setq rust-format-on-save t))
+;; ace-window
+(use-package ace-window)
 
-;; go-mode
-(use-package go-mode
-  :ensure t
+;; company-mode
+(use-package company
   :config
-  (setq gofmt-command "goimports")
-  (add-hook 'before-save-hook 'gofmt-before-save))
+  (company-mode 1))
 
-;; toml-mode
-(use-package toml-mode :ensure t)
+;; smooth-scrolling
+(use-package smooth-scrolling
+  :config
+  (smooth-scrolling-mode 1))
 
-;; yaml-mode
-(use-package yaml-mode :ensure t)
 
-;; scala-mode
-(use-package scala-mode :ensure t)
-
-;; haskell-mode
-(use-package haskell-mode
-  :ensure t)
-
-;; json-mode
-(use-package json-mode :ensure t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Project management packages:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; magit
 (use-package magit
-  :ensure t
   :config
   (global-set-key (kbd "C-x g") 'magit-status)
   (global-set-key (kbd "C-x C-g") 'magit-dispatch-popup)
-  (setq vc-handled-backends nil)) ; disable build in VC management
+  (setq vc-handled-backends nil)) ; disable built in VC management
 
 ;; git-link
 (use-package git-link
-  :ensure t
   :config
   (add-to-list 'git-link-remote-alist '("git\\.corp\\.stripe\\.com" git-link-github))
   (add-to-list 'git-link-commit-remote-alist '("git\\.corp\\.stripe\\.com" git-link-commit-github)))
 
 ;; ivy, counsel, swiper
 (use-package counsel
-  :ensure t
   :config
   (ivy-mode 1)
   (setq ivy-use-virtual-buffers t
@@ -126,43 +145,50 @@
   (global-set-key (kbd "C-x f") 'counsel-git))
 
 ;; projectile
-(use-package projectile :ensure t)
+(use-package projectile)
 
 ;; counsel-projectile
 (use-package counsel-projectile
-  :ensure t
   :config
   (counsel-projectile-mode 1))
 
 ;; flycheck
 (use-package flycheck
-  :ensure t
   :config
   (global-flycheck-mode 1))
 
-;; avy
-(use-package avy :ensure t)
 
-;; ace-window
-(use-package ace-window :ensure t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Language support packages:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; company-mode
-(use-package company
-  :ensure t
+;; elm-mode
+(use-package elm-mode
+  :config (setq elm-format-on-save t))
+
+;; rust-mode
+(use-package rust-mode
+  :config (setq rust-format-on-save t))
+
+;; go-mode
+(use-package go-mode
   :config
-  (company-mode 1))
+  (setq gofmt-command "goimports")
+  (add-hook 'before-save-hook 'gofmt-before-save))
 
-;; evil --- must resist for now
-;; (use-package evil :ensure t :config (evil-mode 1))
+;; toml-mode
+(use-package toml-mode)
 
-;; smooth-scrolling
-(use-package smooth-scrolling
-  :ensure t
-  :config
-  (smooth-scrolling-mode 1))
+;; yaml-mode
+(use-package yaml-mode)
 
-;; better scrolling defaults
-(setq scroll-margin 10
-      scroll-preserve-screen-position 1)
+;; scala-mode
+(use-package scala-mode)
+
+;; haskell-mode
+(use-package haskell-mode)
+
+;; json-mode
+(use-package json-mode)
 
 ;;; init.el ends here
