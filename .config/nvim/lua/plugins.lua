a5a81d57f592795db7f00327fa60d7cb3a3b378a
Jerry Tegno <jerrytegno@gmail.com>
2022-05-23 20:10:39 -0700

    chair

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -253,6 +253,9 @@ cmp.setup({
     },
 })
 
+-- when deleting text, enter insert mode instead of bailing out to normal mode
+u.map("s", "<BS>", "<BS>i")
+
 -- properly insert braces after completing functions
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
