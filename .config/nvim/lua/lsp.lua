78366b90785133627f8f109530132bb267b56914
Jerry Tegno <jerrytegno@gmail.com>
2023-02-28 10:29:53 -0800

    sheep

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -151,7 +151,7 @@ lspconfig.vimls.setup({
 })
 
 -- Lua
-lspconfig.sumneko_lua.setup({
+lspconfig.lua_ls.setup({
     on_attach = function(client, bufnr)
         on_attach(client, bufnr)
 
@@ -160,6 +160,27 @@ lspconfig.sumneko_lua.setup({
     end,
     handlers = handlers,
     capabilities = capabilities,
+
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 })
 
 -- Rust
