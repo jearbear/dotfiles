8df873659a9b1e4c1c8bbeec7ac4be42509ae9dc
Jerry Tegno <jerrytegno@gmail.com>
2025-10-01 22:39:10 -0700

    lyric

--- .config/fish/linux.fish
+++ .config/fish/linux.fish
@@ -1,6 +1,6 @@
 if status is-interactive
-    abbr ne 'nvim ~/.nixos/configuration.nix'
-    abbr nb 'sudo nixos-rebuild switch --flake ~/.nixos'
+    abbr ne 'nvim ~/.config/nixos/configuration.nix'
+    abbr nb 'sudo nixos-rebuild switch --flake ~/.config/nixos'
     abbr nr --set-cursor 'nix run nixpkgs#%'
     abbr ns 'nix search nixpkgs'
     abbr npi --set-cursor 'nix path-info nixpkgs#%'
