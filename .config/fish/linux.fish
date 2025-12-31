5ab0ad29ccfae924c0b44de8969c0241dec29c35
Jerry Tegno <jerrytegno@gmail.com>
2025-12-30 19:47:33 -0800

    argue

--- .config/fish/linux.fish
+++ .config/fish/linux.fish
@@ -1,8 +1,5 @@
-set -gx EDITOR k
-set -gx VISUAL k # use kakoune to edit command buffer
-
 if status is-interactive
-    abbr ne 'k ~/.config/nixos/configuration.nix'
+    abbr ne 'nvim ~/.config/nixos/configuration.nix'
     abbr nb 'sudo nixos-rebuild switch --flake ~/.config/nixos'
     abbr nr --set-cursor 'nix run nixpkgs#%'
     abbr ns 'nix search nixpkgs'
